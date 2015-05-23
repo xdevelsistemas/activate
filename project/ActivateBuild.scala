506f6cb3cefe5ca013e75e7e942e15403d4e3eb1
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-05-23 20:04:40 -0300

    release version 1.7-xdevel

--- project/ActivateBuild.scala
+++ project/ActivateBuild.scala
@@ -268,7 +268,7 @@ object ActivateBuild extends Build {
     def commonSettings =
         Defaults.defaultSettings ++ Seq(
             organization := "net.fwbrasil",
-            version := "1.7",
+            version := "1.7-xdevel",
             scalaVersion := "2.11.6",
             javacOptions ++= Seq("-source", "1.7", "-target", "1.7"),
             publishMavenStyle := true,
